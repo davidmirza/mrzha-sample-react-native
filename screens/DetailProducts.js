36acc0c9afe1daebec1d280b17c386b1061d7c5c
davidmirza <davemirza9@gmail.com>
2023-12-30 21:56:15 +0700

    update detail page

--- screens/DetailProducts.js
+++ screens/DetailProducts.js
@@ -13,7 +13,10 @@ import { Feather } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const DetailProducts = () => {
   const route = useRoute();
@@ -116,15 +119,7 @@ const DetailProducts = () => {
           </View>
         </View>
         <View>
-          <Text
-            style={{
-              height: 8,
-              borderColor: "#D0D0D0",
-              backgroundColor: "#D0D0D0",
-              borderWidth: 1,
-              marginTop: 0,
-            }}
-          />
+          <Text style={styles.space} />
         </View>
         <View style={{ padding: 5, flexDirection: "column" }}>
           <View>
@@ -139,7 +134,7 @@ const DetailProducts = () => {
               styles.colorDark,
               {
                 padding: 7,
-                marginBottom: 100,
+                marginBottom: 5,
                 backgroundColor: "#fafcfb",
                 borderRadius: 5,
                 borderWidth: 1,
@@ -150,6 +145,335 @@ const DetailProducts = () => {
             <Text>{route.params.description}</Text>
           </View>
         </View>
+        <View>
+          <Text style={styles.space} />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <View style={{ flexDirection: "row" }}>
+            <View
+              style={{
+                marginLeft: 15,
+                height: 80,
+                width: 80,
+                alignContent: "center",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  borderWidth: 1,
+                  borderRadius: 40,
+                  borderColor: "#d7dbd9",
+                  padding: 10,
+                  height: 70,
+                  width: 70,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <MaterialIcons name="storefront" size={35} color="#05fa63" />
+              </TouchableOpacity>
+            </View>
+
+            <View
+              style={{
+                marginLeft: 10,
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="star-circle"
+                  size={18}
+                  color="#06c25a"
+                />
+                <Text
+                  style={{
+                    marginLeft: 3,
+                    marginTop: -1,
+                    fontSize: 16,
+                    fontWeight: "700",
+                  }}
+                >
+                  Sample Store
+                </Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "flex-start",
+                  marginTop: 2,
+                }}
+              >
+                <Text
+                  style={{
+                    marginTop: 1,
+                    marginLeft: 8,
+                    fontSize: 11,
+                    fontWeight: "300",
+                  }}
+                >
+                  online
+                </Text>
+                <Text
+                  style={{
+                    marginLeft: 3,
+                    marginTop: -1,
+                    fontSize: 12,
+                    fontWeight: "500",
+                  }}
+                >
+                  1 hr ago
+                </Text>
+              </View>
+              <Text
+                style={{
+                  marginTop: -1,
+                  marginLeft: 8,
+                  fontSize: 12,
+                  fontWeight: "400",
+                }}
+              >
+                Semarang, City
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "column",
+              marginStart: 25,
+              marginBottom: 5,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <AntDesign name="staro" size={16} color="gray" />
+              <Text
+                style={{
+                  marginLeft: 3,
+                  marginTop: 0,
+                  fontSize: 14,
+                  fontWeight: "500",
+                }}
+              >
+                4.6
+              </Text>
+              <Text
+                style={{
+                  marginLeft: 3,
+                  marginTop: 0,
+                  fontSize: 12,
+                  fontWeight: "400",
+                  color: "gray",
+                }}
+              >
+                average review
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginLeft: 2,
+              }}
+            >
+              <AntDesign name="clockcircleo" size={13} color="gray" />
+              <Text
+                style={{
+                  marginLeft: 3,
+                  marginTop: -2,
+                  fontSize: 14,
+                  fontWeight: "500",
+                  textDecorationLine: "underline",
+                }}
+              >
+                +
+              </Text>
+              <Text
+                style={{
+                  marginLeft: 3,
+                  marginTop: 0,
+                  fontSize: 14,
+                  fontWeight: "500",
+                }}
+              >
+                2 hours
+              </Text>
+              <Text
+                style={{
+                  marginLeft: 3,
+                  marginTop: 0,
+                  fontSize: 12,
+                  fontWeight: "400",
+                  color: "gray",
+                }}
+              >
+                order processed
+              </Text>
+            </View>
+          </View>
+          <TouchableOpacity>
+            <View
+              style={{
+                marginRight: 15,
+                flexDirection: "row",
+                borderWidth: 1,
+                borderRadius: 10,
+                borderColor: "#06c25a",
+                padding: 8,
+              }}
+            >
+              <SimpleLineIcons
+                name="user-follow"
+                style={{ color: "#05fa63", fontSize: 18, marginRight: 6 }}
+              />
+              <Text style={{ color: "#06c25a", fontSize: 13 }}>Follow</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text style={styles.space} />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <View style={{ flexDirection: "row", marginLeft: 25 }}>
+            <View
+              style={{
+                marginTop: 10,
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <FontAwesome5 name="shipping-fast" size={18} color="#06c25a" />
+
+                <Text
+                  style={{
+                    marginLeft: 5,
+                    marginTop: 0,
+                    fontSize: 14,
+                    color: "gray",
+                    fontWeight: "500",
+                  }}
+                >
+                  Shipping Cost
+                </Text>
+                <Text
+                  style={{
+                    marginLeft: 3,
+                    marginTop: 0,
+                    fontSize: 16,
+                    fontWeight: "700",
+                  }}
+                >
+                  $5
+                </Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "flex-start",
+                  marginTop: 2,
+                }}
+              >
+                <Text
+                  style={{
+                    marginLeft: 8,
+                    fontSize: 12,
+                    fontWeight: "300",
+                  }}
+                >
+                  Estimation
+                </Text>
+                <Text
+                  style={{
+                    marginLeft: 3,
+                    marginTop: -1,
+                    fontSize: 12,
+                    fontWeight: "500",
+                  }}
+                >
+                  1 - 3 Days
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginBottom: 5,
+            marginLeft:25,
+            marginRight:25
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{
+                marginLeft: 8,
+                marginRight: 8,
+                fontSize: 12,
+                fontWeight: "400",
+              }}
+            >
+              Courier :
+            </Text>
+            <MaterialCommunityIcons name="bike-fast" size={14} color="black" />
+            <Text style={{ backgroundColor: "#c7c9c5", marginStart: 5 }}>
+              {" "}COD{" "}
+            </Text>
+          </View>
+          <View>
+            <Feather name="chevron-right" size={24} color="black" />
+          </View>
+        </View>
+        <View>
+          <Text style={styles.space} />
+        </View>
+        <View style={{ height: 20 }}></View>
       </ScrollView>
       <View
         style={{
@@ -160,13 +484,11 @@ const DetailProducts = () => {
         }}
       >
         <TouchableOpacity>
-            <View
-               style={{borderWidth:0, padding:2, borderRadius:8}}
-            >
-               <Ionicons name="chatbox-ellipses-outline" style={styles.icnCart} />
-            </View>
-          </TouchableOpacity>
-       
+          <View style={{ borderWidth: 0, padding: 2, borderRadius: 8 }}>
+            <Ionicons name="chatbox-ellipses-outline" style={styles.icnCart} />
+          </View>
+        </TouchableOpacity>
+
         <View
           style={{
             flexDirection: "row",
@@ -183,18 +505,36 @@ const DetailProducts = () => {
                 { backgroundColor: "white", borderColor: "#d7dbd9" },
               ]}
             >
-              <Text style={{fontSize:14,fontWeight:"700", color:"#06c25a"}}>Quick Buy</Text>
+              <Text
+                style={{ fontSize: 14, fontWeight: "700", color: "#06c25a" }}
+              >
+                Quick Buy
+              </Text>
             </View>
           </TouchableOpacity>
           <TouchableOpacity>
             <View
               style={[
                 styles.btnBuy,
-                { backgroundColor: "#06c25a", borderColor: "#03a84d", flexDirection:"row"  },
+                {
+                  backgroundColor: "#06c25a",
+                  borderColor: "#03a84d",
+                  flexDirection: "row",
+                },
               ]}
             >
-                <MaterialCommunityIcons name="cart-plus" style={{fontSize:20,fontWeight:"700", color:"white", marginRight:5}} />
-              <Text style={{fontSize:14,fontWeight:"700", color:"white"}}>Add to Cart</Text>
+              <MaterialCommunityIcons
+                name="cart-plus"
+                style={{
+                  fontSize: 20,
+                  fontWeight: "700",
+                  color: "white",
+                  marginRight: 5,
+                }}
+              />
+              <Text style={{ fontSize: 14, fontWeight: "700", color: "white" }}>
+                Add to Cart
+              </Text>
             </View>
           </TouchableOpacity>
         </View>
@@ -206,9 +546,15 @@ const DetailProducts = () => {
 export default DetailProducts;
 
 const styles = StyleSheet.create({
+  space: {
+    height: 8,
+    borderColor: "#D0D0D0",
+    backgroundColor: "#D0D0D0",
+    borderWidth: 1,
+    marginTop: 0,
+  },
   btnBuy: {
     borderWidth: 1,
-
     paddingLeft: 15,
     borderRadius: 6,
     paddingRight: 15,
